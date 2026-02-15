4ac826db2dee3d1ade24cc4d39b2dff8ff9d9f17
aroa262004-ai <aroa262004@gmail.com>
2026-02-15 18:30:37 +0100

    añadir diseño y estructura HTML

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def inicio():
-    return "<h1>El mejor proyecto de LPRO</h1>"
+    return render_template("index.html")
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
